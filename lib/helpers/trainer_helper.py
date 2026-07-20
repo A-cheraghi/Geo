95a6430cb7a2830ae1b1d216b39d66e7f0e36815
A-cheraghi <cheraghiali4@gmail.com>
2026-07-21 02:08:14 +0330

    شش

--- lib/helpers/trainer_helper.py
+++ lib/helpers/trainer_helper.py
@@ -208,8 +208,9 @@ class Trainer(object):
 
             detr_losses.backward()
             self.optimizer.step()
-            if (batch_idx % 200 ==0):
-                progress_bar.update()
+
+            if batch_idx % 200 == 0:
+                progress_bar.update(200)
         progress_bar.close()
 
     def prepare_targets(self, targets, batch_size):
