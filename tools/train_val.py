793803a6091290d8da3e298d678bbcd26171b85c
A-cheraghi <cheraghiali4@gmail.com>
2026-07-24 15:14:52 +0330

    نسیبس

--- tools/train_val.py
+++ tools/train_val.py
@@ -5,6 +5,7 @@ warnings.filterwarnings("ignore")
 import os
 import sys
 import torch
+import torch.nn as nn
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
